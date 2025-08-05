1eae37879e79661cfa9ff01ab448a4a015788b6f
evangelostsak <baggtsak55@gmail.com>
2025-08-05 21:59:01 +0200

    test broken code commit

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"testing"
+	"net/http"
+)
+
+func TestGetAPIKey_ValidHeader(t *testing.T) {
+	headers := http.Header{}
+	headers.Set("Authorization", "ApiKey 123456")
+	apiKey, err := GetAPIKey(headers)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	if apiKey != "123456" {
+		t.Errorf("expected apiKey '123456', got '%s'", apiKey)
+	}
+}
